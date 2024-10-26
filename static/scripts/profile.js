dafa4a268037efc67da4741e478fd52fb8933a21
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2024-10-27 00:34:37 +0300

    Visual Icon for submitting an email for the newsletter

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -1,33 +1,36 @@
-    document.querySelector('.newsletter-form').addEventListener('submit', async function(event) {
-        event.preventDefault();
+const newsletterSuccessIcon = document.getElementById('newsletter-submit-success');
 
-        const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
-        const emailInput = document.querySelector('input[name="newsletter-email-input"]').value;
-        const url = this.getAttribute('data-url');
-        console.log(url)
+document.querySelector('.newsletter-form').addEventListener('submit', async function(event) {
+    event.preventDefault();
 
-        const data = {
-            'client_id_input':  logged_user,
-            'newsletter-email-input': emailInput
-        };
-        console.log("HERE5")
-        try {
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrfToken
-                },
-                body: JSON.stringify(data)
-            });
 
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                console.log('Success:', jsonResponse);
-            } else {
-                console.error('Error:', response.statusText);
-            }
-        } catch (error) {
-            console.error('Fetch error:', error);
+    const csrfToken = document.querySelector('input[name="csrfmiddlewaretoken"]').value;
+    const emailInput = document.querySelector('input[name="newsletter-email-input"]').value;
+    const url = this.getAttribute('data-url');
+
+    const data = {
+        'client_id_input':  logged_user,
+        'newsletter-email-input': emailInput
+    };
+    console.log("HERE5")
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-CSRFToken': csrfToken
+            },
+            body: JSON.stringify(data)
+        });
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            this.style.display = "none";
+            newsletterSuccessIcon.style.display = "flex";
+        } else {
+            console.error('Error:', response.statusText);
         }
-    });
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+});
