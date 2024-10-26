4bed5682fcc05e49ae80b7723d03413d654f83e8
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2024-10-26 22:04:01 +0300

    Added newsletter section, solved out of range slide of the carousels problem

--- static/scripts/home.js
+++ static/scripts/home.js
@@ -2,13 +2,21 @@ let currentSlide = 0;
 
 function showSlide(index) {
     const slides = document.querySelectorAll('.carousel-images img');
-    if (index >= slides.length) {
+
+    if (window.innerWidth >= 1200) {
+        slideLimit = slides.length / 3;
+    } else if (window.innerWidth >= 900) {
+        slideLimit = slides.length / 2;
+    } else {
+        slideLimit = slides.length;
+    }
+
+    if (index < 0 || index >= slideLimit) {
         currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = slides.length - 1;
     } else {
         currentSlide = index;
     }
+
 
     const offset = -currentSlide * 100; // Each slide takes 100% width
     document.querySelector('.carousel-images').style.transform = `translateX(${offset}%)`;
@@ -16,6 +24,35 @@ function showSlide(index) {
 
 function moveSlide(step) {
     showSlide(currentSlide + step);
+}
+
+// Initial call to display the first slide
+showSlide(currentSlide);
+
+
+function showFeedbackSlide(index) {
+    const slides = document.querySelectorAll('.feedback-images img');
+
+    if (window.innerWidth >= 1200) {
+        slideLimit = slides.length / 3;
+    } else if (window.innerWidth >= 900) {
+        slideLimit = slides.length / 2;
+    } else {
+        slideLimit = slides.length;
+    }
+
+    if (index < 0 || index >= slideLimit) {
+        currentSlide = 0;
+    } else {
+        currentSlide = index;
+    }
+
+    const offset = -currentSlide * 100; // Each slide takes 100% width
+    document.querySelector('.feedback-images').style.transform = `translateX(${offset}%)`;
+}
+
+function moveFeedbackSlide(step) {
+    showFeedbackSlide(currentSlide + step);
 }
 
 // Initial call to display the first slide
